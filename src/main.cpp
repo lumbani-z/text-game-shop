89d8512f7ebd38d958f270d7a7017c7b84c53c7c
lumbani-z <lumbani@ualberta.ca>
2025-04-19 23:13:31 -0600

    Added selection sfx

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,12 @@ struct Shop{
 
 // This is my main function
 int main() {
+    sf::SoundBuffer selectBuffer;
+    if (!selectBuffer.loadFromFile("fx.wav")) { // Replace with your sound file path
+        std::cout << "Error: Could not load sound file!" << std::endl;
+        return -1;
+    }
+    sf::Sound selectSound(selectBuffer);
     // Load background music
     sf::Music music;
     if (!music.openFromFile("24.wav")) { // Replace with your music file path
@@ -56,6 +62,7 @@ int main() {
     // We ask the player for their name
     std::cout << "Before we begin, what is your name? ";
     std::cin >> player.name;
+    selectSound.play();
 
     // We greet the player
 
@@ -82,21 +89,23 @@ int main() {
             invalidChoice = false; // Reset the invalidChoice flag
         }
         std::cin >> choice; // Logs the user's choice
-
-
+        selectSound.play(); // Play the sound effect
         // Checks the user's choice and diplays one of the following messages
         if (choice == 1){
                 bool buying = true;
                 bool noGold = false;
                 bool outOfStock = false;
                 bool succesfulPurchase = false;
+                int healthPotionStock = shop.numHealthPotions;
+                int manaPotionStock = shop.numManaPotions;
+                int swordStock = shop.numSwords;
                 while(buying){
                     system("cls");
                     std::cout << "Available products!" << std::endl;
                     std::cout << "--------------------------------------------------------" << std::endl;
-                    std::cout << "1. Health Potion - 10 Gold" << std::endl;
-                    std::cout << "2. Mana Potion - 10 Gold" << std::endl;
-                    std::cout << "3. Sword - 10 Gold" << std::endl;
+                    std::cout << "1. Health Potion - 10 Gold [" << shop.numHealthPotions << " Left]" << std::endl;
+                    std::cout << "2. Mana Potion - 10 Gold [" << shop.numManaPotions << " Left]"  << std::endl;
+                    std::cout << "3. Sword - 10 Gold [" << shop.numSwords << " Left]"  << std::endl;
                     std::cout << "4. Back" << std::endl;
                     std::cout << "Please enter your choice [1-4]: " << std::endl;
                     if (noGold){
@@ -117,14 +126,15 @@ int main() {
                     }
 
                     std::cin >> nestedChoice; // Logs the user's choice
+                    selectSound.play();
                     if (std::cin.fail()){
                         std::cin.clear(); // Clear the error flag
                         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the invalid input
+                        invalidChoice = true; // Set the invalidChoice flag
                     }
                     else if (nestedChoice == 1){
                         if (player.gold >= shop.potionCost){
                             if (shop.numHealthPotions > 0){
-                                std::cout << "You have purchased a health potion!" << std::endl;
                                 player.inventory[player.inventoryCount] = "Health Potion";
                                 player.inventoryCount++;
                                 player.gold -= shop.potionCost;
@@ -143,7 +153,6 @@ int main() {
                     else if (nestedChoice == 2){
                         if (player.gold >= shop.potionCost){
                             if (shop.numManaPotions > 0){
-                                std::cout << "You have purchased a mana potion!" << std::endl;
                                 player.inventory[player.inventoryCount] = "Mana Potion";
                                 player.inventoryCount++;
                                 player.gold -= shop.potionCost;
@@ -162,7 +171,6 @@ int main() {
                     else if (nestedChoice == 3){
                         if (player.gold >= shop.potionCost){
                             if (shop.numSwords > 0){
-                                std::cout << "You have purchased a Sword!" << std::endl;
                                 player.inventory[player.inventoryCount] = "Sword";
                                 player.inventoryCount++;
                                 player.gold -= shop.swordCost;
@@ -221,6 +229,7 @@ int main() {
                 }
                 std::cout << "Enter [1] to go back" << std::endl;
                 std::cin >> nestedChoice; 
+                selectSound.play();
                 if (std::cin.fail()){
                     std::cin.clear(); // Clear the error flag
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignore the invalid input
